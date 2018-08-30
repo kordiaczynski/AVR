203ac07a2c633a0ac8546a1b893d905af7def7e0
Szymon Kordiaczyński <kordiaczynski@Szymons-MacBook-Pro.local>
2018-08-30 21:58:47 +0200

    I've removed some unnecessary parts of code, and add some comments

--- UARt/main.c
+++ UARt/main.c
@@ -10,7 +10,6 @@
 
 ISR(USART_RXC_vect, ISR_BLOCK )
 {
- //while ( ! (UCSRA & ( 1 << RXC )) == 0 ) {};
  unsigned char received = UDR;
  if( received == 'a' )
  {
@@ -28,27 +27,11 @@ int main()
 {
 	DDRB = 3; // ( 1 << PB0 ) | (1 << PB1);
 	PORTB = 1;	
-	PORTD = 2;
 	uart_init();
 	UCSRB |= (1 << RXCIE);
 	sei();
 	while(1)
 	{
-/*		unsigned char received = uart_receive();
-		if( received == 'a' )
-		{
-
-
-			PORTB &= ~_BV(PB0);
-			_delay_ms(300);
-		}
-		else
-		{
-
-			PORTB |= _BV(PB0);
-
-		}
-*/
 	}
 	return 0;
 }
--- UARt/sender.c
+++ UARt/sender.c
@@ -5,34 +5,26 @@
 
 int main()
 {
-	
-	PORTD  = 2;
 	uart_init();
-	
 	unsigned int input = 0x1;
-	unsigned int output = 0x0;
+	unsigned int output = 0x2;                      //I had to change that because I've broken one pin
 	
-	DDRB   = 0x00;
-	DDRB  |=  (1 << output );
-	PORTB |=  (1 << input );
+	DDRB   = 0x00;                                  //All pins as input
+	DDRB  |=  (1 << output );			//Set pin PB0 as output -1
+	PORTB |=  (1 << input );			//Connet pin PB1 ( input because of 0 ) to pull up resistor
 
-
-		
-	while(1)
+	_delay_ms(1000); 				//Every time when I put the power on sender it 		
+	while(1)					//think that button is pressed so I wait for a monent
 	{	
 		
-		if(( PINB & ( 1<< input )) == 0 )
+		if(( PINB & ( 1<< input )) == 0 )       //Zero means neutral cabel connected
 		{
-
-			PORTB |= 1;
+			PORTB &= ~_BV(output);                    //Zero means connect neutral to this out
 			uart_transmit('a');		
 		}
 		else
 		{
-
-			PORTB &= ~1;
-			
-
+			PORTB |= _BV(output);			
 		}
 		
 		
